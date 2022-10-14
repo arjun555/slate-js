a30e59df6b90877d091ee58999a948d06e590e23
Arjun Patel <arjun@myxplor.com>
2022-10-14 15:56:21 +1100

    slate boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+// TypeScript users only add this code
+import { BaseEditor, Descendant } from "slate";
+import { ReactEditor } from "slate-react";
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
 }
+
+// Add the initial value.
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()));
+
+  const handleOnChange = (value: Descendant[]) => {
+    const isStateChange = editor.operations.some(
+      (op) => "set_selection" !== op.type
+    );
+
+    if (isStateChange) {
+      const content = JSON.stringify(value);
+      localStorage.setItem("content", content);
+    }
+  };
+
+  return (
+    <Slate editor={editor} value={initialValue} onChange={handleOnChange}>
+      <Editable />
+    </Slate>
+  );
+};
 
 export default App;
